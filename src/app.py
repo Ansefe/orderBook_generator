a4e6b3b47574559b0268b38c533e58f219d2e52a
Andres Caicedo <caicedoleon.andresfelipe@gmail.com>
2023-05-04 22:43:16 -0500

    gunicorn first test

--- src/app.py
+++ src/app.py
@@ -17,7 +17,6 @@ cursor.execute('''CREATE TABLE orders
                   (id INTEGER PRIMARY KEY, 
                    book BLOB)''')
 
-app = Flask(__name__)
 
 # define las variables necesarias
 symbol = "BTCUSDT"
@@ -103,40 +102,18 @@ def on_open(ws):
     ws.send(json.dumps(payload))
 
 ################################################################################################################
-@app.route('/order-book')
-def order_book():
-    # Consulta a la tabla
-    cursor.execute("SELECT book FROM orders WHERE id=1")
+def create_app():
+    app = Flask(__name__)
+    @app.route('/order-book')
+    def order_book():
+        # Consulta a la tabla
+        cursor.execute("SELECT book FROM orders WHERE id=1")
 
-    # Recuperar el objeto serializado
-    serialized_book = cursor.fetchone()[0]
+        # Recuperar el objeto serializado
+        serialized_book = cursor.fetchone()[0]
 
-    # Deserialización del objeto
-    order_book = pickle.loads(serialized_book)
-    return jsonify({'orderBook': order_book})
-
-# Define la función para ejecutar la conexión al websocket
-def run_websocket():
-    ws = websocket.WebSocketApp(ws_url,
-                                on_message=process_message,
-                                on_open=on_open,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.run_forever()
-
-# Define la función para ejecutar la aplicación Flask
-def run_flask():
-    app.run()
-
-if __name__ == "__main__":
-    # Inicia un proceso en segundo plano para la conexión al websocket
-    websocket_process = Process(target=run_websocket)
-    # Inicia un proceso en segundo plano para la aplicación Flask
-    flask_process = Process(target=run_flask)
+        # Deserialización del objeto
+        order_book = pickle.loads(serialized_book)
+        return jsonify({'orderBook': order_book})
     
-    websocket_process.start()
-    flask_process.start()
-    
-    # Espera a que los procesos terminen
-    websocket_process.join()
-    flask_process.join()
+    return app
